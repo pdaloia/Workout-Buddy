7470c6bd79a2a8242594cec2015265d8ea212350
Philip D'Aloia <philip.daloia@gmail.com>
2021-12-24 10:35:46 -0500

    Adding model for a set

---
+++ Workout Buddy/Models/Set.swift
@@ -0,0 +1,22 @@
+//
+//  Set.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2021-12-24.
+//
+
+import Foundation
+
+class Set {
+    
+    var reps: Int
+    var weight: Double
+    
+    init(reps: Int, weight: Double) {
+        
+        self.reps = reps
+        self.weight = weight
+        
+    }
+    
+}
