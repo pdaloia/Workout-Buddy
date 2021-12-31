a595dca78b811afad90e31ff3d4974e413dc897e
Philip D'Aloia <philip.daloia@gmail.com>
2021-12-31 14:34:53 -0500

    Creating VC and View for creating a set

---
+++ Workout Buddy/Controllers/Create Routine Controllers/CreateSetViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreateSetViewController.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2021-12-31.
+//
+
+import UIKit
+
+class CreateSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Workout Buddy/Views/Create Routine Views/CreateSetView.swift
@@ -0,0 +1,16 @@
+//
+//  CreateSetView.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2021-12-31.
+//
+
+import UIKit
+
+class CreateSetView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+}
