c261f6553a96e7f002277f46b7c5d806f4bf2f0e
Philip D'Aloia <philip.daloia@gmail.com>
2022-01-02 19:53:46 -0500

    Creating the VC and View for creating a workout

---
+++ Workout Buddy/Controllers/Create Routine Controllers/CreateWorkoutViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateWorkoutViewController.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2022-01-02.
+//
+
+import UIKit
+
+class CreateWorkoutViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    let mainView: CreateWorkoutView = {
+        let view = CreateWorkoutView()
+        return view
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+        
+    }
+    
+    //MARK: - Functions
+    
+    func initializeView() {
+        
+        self.view.addSubview(self.mainView)
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        mainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+    }
+
+}
---
+++ Workout Buddy/Views/Create Routine Views/CreateWorkoutView.swift
@@ -0,0 +1,26 @@
+//
+//  CreateWorkoutView.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2022-01-02.
+//
+
+import UIKit
+
+class CreateWorkoutView: UIView {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Functions
+
+}
