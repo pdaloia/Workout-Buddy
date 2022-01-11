39fb4694b77e440a503249112a3521938bd9f22c
Philip D'Aloia <philip.daloia@gmail.com>
2022-01-10 20:31:45 -0500

    Adding view components to create routine view

--- Workout Buddy/Views/Create Routine Views/CreateRoutineView.swift
+++ Workout Buddy/Views/Create Routine Views/CreateRoutineView.swift
@@ -11,6 +11,24 @@ class CreateRoutineView: UIView {
 
     //MARK: - Properties
     
+    let routineNameInputLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Enter the name of the routine"
+        return view
+    }()
+    
+    let routineNameInput: UITextField = {
+        let view = UITextField()
+        view.placeholder = "Routine name"
+        return view
+    }()
+    
+    let routineWorkoutsInputLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Select the workouts for this routine"
+        return view
+    }()
+    
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -26,6 +44,27 @@ class CreateRoutineView: UIView {
     //MARK: - Functions
     
     func initializeView() {
+        
+        //add the subviews
+        self.addSubview(routineNameInputLabel)
+        self.addSubview(routineNameInput)
+        self.addSubview(routineWorkoutsInputLabel)
+        
+        //set the contraints
+        routineNameInputLabel.translatesAutoresizingMaskIntoConstraints = false
+        routineNameInputLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        routineNameInputLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+        routineNameInputLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
+        
+        routineNameInput.translatesAutoresizingMaskIntoConstraints = false
+        routineNameInput.topAnchor.constraint(equalTo: routineNameInputLabel.bottomAnchor ).isActive = true
+        routineNameInput.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+        routineNameInput.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
+        
+        routineWorkoutsInputLabel.translatesAutoresizingMaskIntoConstraints = false
+        routineWorkoutsInputLabel.topAnchor.constraint(equalTo: routineNameInput.bottomAnchor, constant: 20).isActive = true
+        routineWorkoutsInputLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+        routineWorkoutsInputLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
         
     }
 
