8b889bcb06be434a1255224e07f069a37921c1dc
Philip D'Aloia <philip.daloia@gmail.com>
2023-02-12 21:48:12 -0500

    Adding view controller files to each tab folder

---
+++ Workout Buddy/Controllers/Info Tab Controllers/InfoTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoTabViewController.swift
+//  Workout Buddy
+//
+//  Created by Philip D'Aloia on 2023-02-12.
+//
+
+import UIKit
+
+class InfoTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
