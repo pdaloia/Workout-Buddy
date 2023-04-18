ad127ccbb605e81353d0a95a322bbf6a3ad1d666
Philip D'Aloia <philip.daloia@gmail.com>
2023-04-18 13:40:42 -0400

    Adding subviews to the routine tab view

--- Workout Buddy/Views/Routines Tab Views/RoutinesTabView.swift
+++ Workout Buddy/Views/Routines Tab Views/RoutinesTabView.swift
@@ -16,7 +16,23 @@ class RoutinesTabView: UIView {
     private lazy var viewLabel: UILabel = {
         let label = UILabel()
         label.text = "Routine Tab"
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var newRoutineNameTextField: UIAnimatedPlaceHolderTextField = {
+        let textField = UIAnimatedPlaceHolderTextField(initialPlaceholder: "Routine Name", editedPlaceholder: "Routine Name")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var newRoutineSaveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Save", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
+        return button
     }()
     
     //MARK: - Lifecycle
@@ -36,9 +52,31 @@ class RoutinesTabView: UIView {
     func initializeView() {
         
         self.addSubview(self.viewLabel)
-        viewLabel.translatesAutoresizingMaskIntoConstraints = false
-        viewLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        viewLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            viewLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            viewLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            viewLabel.heightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.heightAnchor, multiplier: 1/10)
+        ])
+        
+        self.addSubview(self.newRoutineNameTextField)
+        NSLayoutConstraint.activate([
+            newRoutineNameTextField.topAnchor.constraint(equalTo: self.viewLabel.bottomAnchor, constant: 20),
+            newRoutineNameTextField.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            newRoutineNameTextField.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            newRoutineNameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        self.addSubview(newRoutineSaveButton)
+        NSLayoutConstraint.activate([
+            newRoutineSaveButton.topAnchor.constraint(equalTo: newRoutineNameTextField.bottomAnchor, constant: 20),
+            newRoutineSaveButton.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor)
+        ])
+        
+    }
+    
+    @objc func saveButtonClicked() {
+        
+        print("saving!!!")
         
     }
 
